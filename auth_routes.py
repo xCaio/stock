08da67b756a22430e4dd7e1d2232ae883c902e7a
xcaio <caiosantos1703@gmail.com>
2026-07-23 17:08:13 -0300

    Add user account creation endpoint with email validation

--- auth_routes.py
+++ auth_routes.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from dependencies import get_session
+from models import User
 
 auth_router = APIRouter(prefix='/auth', tags=['auth'])
 
@@ -6,4 +8,16 @@ auth_router = APIRouter(prefix='/auth', tags=['auth'])
 async def home():
     return{
         "message": "Rota de autenticação"
+    }
+
+
+@auth_router.post('/create-account')
+async def create_account(name, email, password, session=Depends(get_session)):
+    user = session.query(User).filter(User.email == email).first()
+    if user:
+        raise HTTPException(status_code=400, detail='Email already exists')
+    session.add(User(name, email, password))
+    session.commit()
+    return{
+        "message": f"User created {email}"
     }
