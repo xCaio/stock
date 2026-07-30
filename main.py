6d13c5bc99a9223c7327e0d48746fe2b028eb207
xcaio <caiosantos1703@gmail.com>
2026-07-30 11:52:31 -0300

    Add CORS middleware to enable cross-origin requests

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from supplies_routes import supplies_router
 from auth_routes import auth_router
 from movements_routes import movements_router
@@ -12,3 +13,14 @@ app.include_router(auth_router)
 app.include_router(movements_router)
 app.include_router(dashboard_router)
 app.include_router(users_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://SEU-FRONTEND.vercel.app",  # URL do front em prod 
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
